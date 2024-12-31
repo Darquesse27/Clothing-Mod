10d984eda250ed31db88bf7349991e07ebb957e8
Darquesse27 <darquesse27@gmail.com>
2024-12-31 00:31:23 +0000

    Registered ClothesPieceItem.ClothesPieceItem

--- src/main/java/com/darquesse27/clothingmod/init/ClothingModItems.java
+++ src/main/java/com/darquesse27/clothingmod/init/ClothingModItems.java
@@ -1,11 +1,19 @@
 package com.darquesse27.clothingmod.init;
 
 import com.darquesse27.clothingmod.ClothingMod;
+import com.darquesse27.clothingmod.items.ClothesPieceItem;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ClothingModItems {
+
+
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ClothingMod.MODID);
+
+    public static final DeferredItem<Item> CLOTHES_PIECE_ITEM = ITEMS.register("clothes_piece",
+            () -> new ClothesPieceItem(ClothesPieceItem.PROPERTIES));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/darquesse27/clothingmod/items/ClothesPieceItem.java
@@ -0,0 +1,12 @@
+package com.darquesse27.clothingmod.items;
+
+import net.minecraft.world.item.Item;
+
+public class ClothesPieceItem extends Item {
+
+    public static Properties PROPERTIES = new Item.Properties();
+
+    public ClothesPieceItem(Properties properties) {
+        super(properties);
+    }
+}
